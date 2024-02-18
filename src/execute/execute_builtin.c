78deb8d5dc90e46e6e9fa7d94bef6767b763db59
Alessandra Cruz <alessacode@pm.me>
2024-02-18 19:24:59 -0300

    Adição de duas builtins
    Adição da ft_unset.c e ft_export.c

--- src/execute/execute_builtin.c
+++ src/execute/execute_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_builtin.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: matlopes <matlopes@student.42.rio>         +#+  +:+       +#+        */
+/*   By: acastilh <acastilh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 18:32:33 by acastilh          #+#    #+#             */
-/*   Updated: 2024/02/02 12:51:16 by matlopes         ###   ########.fr       */
+/*   Updated: 2024/02/16 22:23:34 by acastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,18 @@ bool	is_builtin(char *cmd)
 {
 	if (cmd == NULL)
 		return (false);
-	if (strncmp(cmd, "cd", ft_strlen("cd")) == 0)
+	if (ft_strncmp(cmd, "cd", ft_strlen("cd")) == 0)
 		return (true);
-	if (strncmp(cmd, "echo", ft_strlen("echo")) == 0)
+	if (ft_strncmp(cmd, "echo", ft_strlen("echo")) == 0)
 		return (true);
+	if (ft_strncmp(cmd, "env", ft_strlen("env")) == 0)
+		return (true);
+	if (ft_strncmp(cmd, "pwd", ft_strlen("pwd")) == 0)
+        return (true);
+	if (ft_strncmp(cmd, "unset", ft_strlen("unset")) == 0)
+        return (true);
+	if (ft_strncmp(cmd, "export", ft_strlen("export")) == 0)
+        return (true);
 	return (false);
 }
 
@@ -27,9 +35,17 @@ bool	execute_builtin(char **args, t_minishell *shell)
 {
 	if (args == NULL || args[0] == NULL)
 		return (false);
-	if (strcmp(args[0], "cd") == 0)
+	if (ft_strcmp(args[0], "cd") == 0)
 		return (ft_cd(args, shell));
-	if (strcmp(args[0], "echo") == 0)
+	if (ft_strcmp(args[0], "echo") == 0)
 		return (ft_echo(args, shell));
+	if (ft_strcmp(args[0], "env") == 0)
+        return (ft_env(shell));
+	if (ft_strcmp(args[0], "pwd") == 0)
+        return (ft_pwd(shell));
+	if (ft_strcmp(args[0], "unset") == 0)
+        return (ft_unset(args, shell));
+	if (ft_strcmp(args[0], "export") == 0)
+        return (ft_export(args, shell));
 	return (false);
 }
