20afc77df8186bb45044fa35f2cb8d9729576079
Alessandra Cruz <alessacode@pm.me>
2024-02-22 20:16:43 -0300

    Ajuste do ft_unset.c
    A função estava com mais de 25 linhas  e foi necessários ajustar criando uma função auxiliar.

--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -6,42 +6,48 @@
 /*   By: acastilh <acastilh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 17:55:15 by acastilh          #+#    #+#             */
-/*   Updated: 2024/02/16 20:35:27 by acastilh         ###   ########.fr       */
+/*   Updated: 2024/02/22 18:44:04 by acastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-bool    ft_unset(char **args, t_minishell *shell)
+void	remove_env_var(t_envp **l_envp, char *var_name)
 {
-    int i;
-    t_envp *current;
-    t_envp *prev;
+	t_envp	*current;
+	t_envp	*prev;
 
-    i = 1;
-    if (!args || !shell)
-        return (false);
-    while (args[i])
-    {
-        current = shell->l_envp;
-        prev = NULL;
-        while (current)
-        {
-            if (ft_strcmp(current->name, args[i]) == 0)
-            {
-                if (prev)
-                    prev->next = current->next;
-                else
-                    shell->l_envp = current->next;
-                free(current->name);
-                free(current->value);
-                free(current);
-                break;
-            }
-            prev = current;
-            current = current->next;
-        }
-        i++;
-    }
-    return (true);
+	current = *l_envp;
+	prev = NULL;
+	while (current)
+	{
+		if (ft_strcmp(current->name, var_name) == 0)
+		{
+			if (prev)
+				prev->next = current->next;
+			else
+				*l_envp = current->next;
+			free(current->name);
+			free(current->value);
+			free(current);
+			break ;
+		}
+		prev = current;
+		current = current->next;
+	}
+}
+
+bool	ft_unset(char **args, t_minishell *shell)
+{
+	int	i;
+
+	i = 1;
+	if (!args || !shell)
+		return (false);
+	while (args[i])
+	{
+		remove_env_var(&(shell->l_envp), args[i]);
+		i++;
+	}
+	return (true);
 }
